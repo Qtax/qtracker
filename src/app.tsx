6af5c85025abb54e4a1e3afa4ee00a271ae346f1
Qtax <qtax@qtax.se>
2023-09-03 20:13:13 +0200

    State without design

---
+++ src/app.tsx
@@ -0,0 +1,68 @@
+import './app.css';
+import { useEntries } from './state';
+import type { Entry, Aches } from "./state";
+
+
+function App() {
+	let { entries, addAche, remove } = useEntries();
+
+	return (
+		<div className="App">
+			<ul>
+				{entries.map(entry => <li><Entry key={entry.id} entry={entry} remove={() => remove(entry.id)}/></li>)}
+			</ul>
+
+			<p>
+				Headache:
+				<button onClick={() => addAche("headache", "green")}>Green</button>
+				<button onClick={() => addAche("headache", "yellow")}>Yellow</button>
+				<button onClick={() => addAche("headache", "red")}>Red</button> 
+			</p>
+
+			<p>
+				Stomachache:
+				<button onClick={() => addAche("stomachache", "green")}>Green</button>
+				<button onClick={() => addAche("stomachache", "yellow")}>Yellow</button>
+				<button onClick={() => addAche("stomachache", "red")}>Red</button> 
+			</p>
+
+			<p>
+				Menstrual cramps:
+				<button onClick={() => addAche("menstrualCramps", "green")}>Green</button>
+				<button onClick={() => addAche("menstrualCramps", "yellow")}>Yellow</button>
+				<button onClick={() => addAche("menstrualCramps", "red")}>Red</button> 
+			</p>
+
+		</div>
+	);
+}
+
+
+function Entry({ entry, remove }: { entry: Entry, remove: () => void }) {
+	let data = entry.data;
+	return <div>
+		{formatDate(entry.timestamp)} {formatTime(entry.timestamp)} <button onClick={remove}>X</button>: {
+		data.type == "headache" || data.type == "stomachache" || data.type == "menstrualCramps"  ? <Ache entry={entry} data={data} /> :
+		data.type
+	}</div>
+}
+
+function formatDate(date: Date) {
+	return `${date.getFullYear()}-${zeroPad(date.getMonth() + 1)}-${zeroPad(date.getDate())}`;
+}
+
+function formatTime(time: Date) {
+	return zeroPad(time.getHours()) + ":" + zeroPad(time.getMinutes());
+}
+
+function zeroPad(n: number, padding = 2): string {
+	return String(n).padStart(padding, "0");
+}
+
+function Ache({ entry, data }: { entry: Entry, data: Aches }) {
+	return <div>{data.type}: {data.level}</div>
+}
+
+
+
+export default App;
---
+++ src/state.tsx
@@ -0,0 +1,137 @@
+import { useEffect, useReducer } from "react";
+
+
+export type Entry = {
+	id: number;
+	timestamp: Date;
+	note?: string | undefined;
+	data: EntryData;
+};
+
+
+export type EntryData = Aches | Painkillers;
+export type Aches = Headache | Stomachache | MenstrualCramps;
+
+
+/**
+ * Green light: I have a headache but I’m able to GO (i.e. "I have a headache but I'm going to work" or "I'm going to look after my kids")
+ * Yellow light: I have to SLOW down (i.e. "I better take it easy or I might not make that meeting later. I have to be cautious.")
+ * Red light: I have to STOP (i.e. "I have to stop what I'm doing, go home from work, go lie down, I can't look after my children", etc.)
+ * 
+ * Src: https://americanheadachesociety.org/news/the-traffic-light-of-headache/
+ */
+export type PainLevel = "green" | "yellow" | "red";
+
+export type Headache = {
+	type: "headache";
+	level?: PainLevel | undefined;
+};
+
+export type Stomachache = {
+	type: "stomachache";
+	level?: PainLevel | undefined;
+};
+
+export type MenstrualCramps = {
+	type: "menstrualCramps"
+	level?: PainLevel | undefined;
+};
+
+export type Painkillers = {
+	type: "painkillers";
+	kind?: "paracetamol" | "ibuprofen" | "paracetamol+ibuprofen";
+}
+
+
+
+export type State = {
+	usedIds: number;
+	entries: Entry[];
+};
+
+let initialState: State = {
+	usedIds: 0,
+	entries: [],
+}
+
+
+export type AddAction = {
+	type: "add";
+	data: EntryData;
+};
+
+export type RemoveAction = {
+	type: "remove";
+	id: number;
+};
+
+export type Action = AddAction | RemoveAction;
+
+
+export function reducer(state: State, action: Action): State {
+	switch(action.type) {
+		case "add": {
+			let id = state.usedIds;
+			let item: Entry = {
+				id,
+				timestamp: new Date(),
+				data: action.data,
+			};
+
+			return {
+				...state,
+				usedIds: id + 1,
+				entries: [...state.entries, item],
+			};
+		}
+		case "remove": {
+			let { id } = action;
+			return {
+				...state,
+				entries: state.entries.filter(entry => entry.id != id),
+			};
+		}
+	}
+	
+	throw new Error("Unknown action");
+}
+
+const STORAGE_KEY = "entriesState";
+
+export function useEntries() {
+	let [state, dispatch] = useReducer(reducer, initialState, initializer);
+
+	useEffect(() => {
+		localStorage.setItem(STORAGE_KEY, JSON.stringify(state, (key, value) => {
+			if (value instanceof Date) return value.toUTCString();
+			return value;
+		}));
+	}, [state]);
+
+	function addAche(type: Aches["type"], level: PainLevel) {
+		dispatch({ type: "add", data: { type, level } });
+	}
+
+	function remove(id: number) {
+		dispatch({ type: "remove", id });
+	}
+
+	return {
+		entries: state.entries,
+		remove,
+		addAche,
+	};
+}
+
+function initializer(initialState: State): State {
+	let stored = localStorage.getItem(STORAGE_KEY);
+	if (!stored) return initialState;
+	return JSON.parse(stored, (key, value) => {
+		if (key == "timestamp" && value) {
+			let date = new Date(value);
+			if (!isNaN(+date)) return date;
+		}
+
+		return value;
+	});
+}
