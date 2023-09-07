cd5350afc2d56f6293220a52f128bbb930c1ebd7
Qtax <qtax@qtax.se>
2023-09-08 01:41:03 +0200

    Add day headers and basic list styling

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,32 @@
-import './app.css';
+import { Fragment } from 'react';
+import './app.scss';
 import { useEntries } from './state';
 import type { Entry, Aches, Painkillers, EntryData } from "./state";
 
+function truncDate(date: Date): Date {
+	let d = new Date(date);
+	d.setHours(0, 0, 0, 0);
+	return d;
+}
 
 function App() {
 	let { entries, addAche, addPainkillers, remove } = useEntries();
+	let prevDate: Date;
 
 	return (
-		<div className="App">
-			<ul>
-				{entries.map(entry => <li><Entry key={entry.id} entry={entry} remove={() => remove(entry.id)}/></li>)}
-			</ul>
+		<div className="app">
+			<div className="entries">
+				{entries.map(entry => {
+					let date = truncDate(entry.timestamp);
+					let renderDate = !prevDate || +prevDate != +date;
+					prevDate = date;
+
+					return <Fragment key={entry.id}>
+						{renderDate && <DateHeader date={date}/>}
+						<Entry entry={entry} remove={() => remove(entry.id)}/>
+					</Fragment>
+				})}
+			</div>
 
 			<p>
 				Headache:
@@ -44,14 +60,33 @@ function App() {
 }
 
 
+function DateHeader({ date }: { date: Date }) {
+	return <h3>{formatDate(date)} {date.toLocaleString('en-us', {  weekday: 'long' })}</h3>
+}
+
+
 function Entry({ entry, remove }: { entry: Entry, remove: () => void }) {
 	let data = entry.data;
-	return <div>
-		{formatDate(entry.timestamp)} {formatTime(entry.timestamp)} <button onClick={remove}>X</button>: {
-		data.type == "headache" || data.type == "stomachache" || data.type == "menstrualCramps" ? <Ache entry={entry} data={data} /> :
-		data.type == "painkillers" ? <Painkillers entry={entry} data={data} /> :
-		(data as EntryData).type
-	}</div>
+	let level = "level" in data ? data.level : undefined;
+
+	return <div className={`entry ${level ?? ""}`}>
+		<FancyTime time={entry.timestamp} />
+
+		{data.type == "headache" || data.type == "stomachache" || data.type == "menstrualCramps" ?
+			<Ache entry={entry} data={data} /> :
+		data.type == "painkillers" ?
+			<Painkillers entry={entry} data={data} /> :
+		(data as EntryData).type}
+		
+		<button className="remove" onClick={remove}>X</button>
+	</div>
+}
+
+function FancyTime({ time }: { time: Date }) {
+	return <span className="time">
+		{zeroPad(time.getHours())}
+		<span className="minutes">:{zeroPad(time.getMinutes())}</span>
+	</span>;
 }
 
 function formatDate(date: Date) {
@@ -67,11 +102,14 @@ function zeroPad(n: number, padding = 2): string {
 }
 
 function Ache({ entry, data }: { entry: Entry, data: Aches }) {
-	return <div>{data.type}: {data.level}</div>
+	return <div className={`ache ${data.level ?? ""}`}>
+		<span className="type">{data.type}</span>{" "}
+		<span className="level">{data.level}</span>
+	</div>
 }
 
 function Painkillers({ entry, data }: { entry: Entry, data: Painkillers }) {
-	return <div>{data.kind ?? data.type}</div>
+	return <div className="painkillers">{data.kind ?? data.type}</div>
 }
 
 
