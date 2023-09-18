2127061dc18644c97b479fafc8789751355bad05
Andreas Zetterlund <qtax@qtax.se>
2023-09-18 07:31:51 +0200

    move adders into a popup

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -2,15 +2,40 @@ import "./style.scss";
 import { useEntries } from "../../state";
 import Entries from "../entries";
 import AcheAdder from "../ache-adder";
+import { useRef, useState } from "react";
+import useOnClickOutside from "../../hooks/use-on-click-outside";
 
 export default function App() {
 	let { entries, addAche, addPainkillers, remove } = useEntries();
 
+	let [isOpen, setIsOpen] = useState(false);
+
+	let ref = useRef<HTMLDivElement>(null);
+	useOnClickOutside(ref, () => {
+		setIsOpen(false);
+	});
+
 	return (
 		<div className="app">
-			<Entries entries={entries} remove={remove} />
+			<div className="content">
+				<Entries entries={entries} remove={remove} />
+			</div>
 
-			<div className="adders">
+			<div className="mini-nav">
+				<div className="branding">
+					<img src="/qtracker.svg" alt="Logo" />
+					qTracker
+				</div>
+				<button className="main-add" onClick={() => setIsOpen(true)}>
+					Add
+				</button>
+			</div>
+
+			<div
+				className="adders"
+				ref={ref}
+				style={{ display: isOpen ? undefined : "none" }}
+			>
 				<div>
 					<button onClick={() => addPainkillers("paracetamol")}>
 						Paracetamol
